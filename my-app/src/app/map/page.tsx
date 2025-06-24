22bee23a3afa3f68cf0f5b43417b46d4fe872ed4
신상용 <kurt-hide@sinsang-yong-ui-MacBookAir.local>
2025-06-24 22:01:32 +0900

    지도 페이지 컴포넌트 및 관련 로직/파일 추가

--- my-app/src/app/map/page.tsx
+++ my-app/src/app/map/page.tsx
@@ -1,4 +1,16 @@
+import { Metadata } from 'next';
 import React from 'react'
+
+
+export const metadata: Metadata = {
+  title: "MapSnap",
+  description: "한손으로 보는 내 주변 맛집, 데이트, 일상생활 등 다양한 장소를 한눈에 찾아보세요.",
+  openGraph: {
+    title: "MapSnap",
+    description: "한손으로 보는 내 주변 맛집, 데이트, 일상생활 등 다양한 장소를 한눈에 찾아보세요.",
+    url: "https://holo-psi.vercel.app/map",
+  },
+};
 
 function page() {
   return (
